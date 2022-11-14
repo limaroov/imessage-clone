05d66d70fe8bfd30510dcfca1e36c55631ec3d6e
Mustapha Limar <58789886+limaroov@users.noreply.github.com>
2022-11-14 20:55:49 +0100

    initialized backend

---
+++ backend/src/graphql/typedefs/index.ts
@@ -0,0 +1,7 @@
+import merge from "lodash.merge";
+
+import userTypeDefs from "./user";
+
+const typeDefs = merge({}, userTypeDefs);
+
+export default typeDefs;
--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -161,7 +161,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "80a7a396a3f3eb65"; }
+/******/ 		__webpack_require__.h = function() { return "d6b98228f14befeb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
